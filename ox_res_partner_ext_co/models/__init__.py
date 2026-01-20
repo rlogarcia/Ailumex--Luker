e341842ead3dc2029cacf6ff721978773efc8b66
Raul Alejandro Carmona Cuellar <114454944+racarmona89947@users.noreply.github.com>
2026-01-20 16:02:59 -0500

    sin modulos

--- ox_res_partner_ext_co/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_city
-from . import res_country
-from . import res_partner_bank
-from . import res_partner_economic_activity
-from . import res_partner_society
-from . import res_partner
